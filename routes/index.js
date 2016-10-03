20a5ca3f19f3dce51018615535dfb750bd3fc2fe
Debrena McEwen <debrenamcewen@gmail.com>
2016-10-03 14:52:50 -0700

    finished routes and added nav

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Charlotte Uprising' });
 });
 
+router.get('/volunteer', function(req, res, next) {
+  res.render('volunteer');
+});
+
+router.get('/donate', function(req, res, next) {
+  res.render('donate');
+});
+
 module.exports = router;
